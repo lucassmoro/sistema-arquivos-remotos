db3c0fd0e237536f381da21dc517c2bc0acba1a7
lucassmoro <lucas.sm.souza@gmail.com>
2025-10-05 04:06:59 -0300

    cache funcionando no java client. mas ainda sem coerencia de cache entre clientes

--- java/src/main/java/com/mycompany/ine5418/Client.java
+++ java/src/main/java/com/mycompany/ine5418/Client.java
@@ -1,12 +1,73 @@
 package com.mycompany.ine5418;
 import com.mycompany.ine5418.client_lib.ClienteSistemaArquivos;
+import java.util.Scanner;
+import java.util.Map;
+import java.lang.Thread;
 
 public class Client {
-    public static void main(String[] args){
-        ClienteSistemaArquivos cliente = new ClienteSistemaArquivos("localhost", 9090);
-        int abreDescritor = cliente.Abre("cliente_java.txt");
-        int bytesEscritos = cliente.Escreve(abreDescritor, 0, "caralho");
-        cliente.Le(abreDescritor, 0, bytesEscritos);
-        cliente.Fecha(abreDescritor);
+    public static void main(String[] args) throws InterruptedException {
+        ClienteSistemaArquivos cliente = new ClienteSistemaArquivos("localhost",9090);
+        Scanner scanner = new Scanner(System.in);
+
+        //int descritor = -1; // guarda o descritor atual do arquivo
+
+        while (true) {
+            System.out.println("\nMENU DO SISTEMA DE ARQUIVOS REMOTO");
+            System.out.println("1 - Abrir arquivo");
+            System.out.println("2 - Ler arquivo");
+            System.out.println("3 - Escrever no arquivo");
+            System.out.println("4 - Fechar arquivo");
+            System.out.println("0 - Sair");
+            System.out.print("Escolha uma opcao: ");
+
+            int opcao = scanner.nextInt();
+            int descritor;
+            scanner.nextLine(); // consome o \n que sobra do nextInt
+
+            switch (opcao) {
+                case 1: // Abre
+                    System.out.print("Digite o nome do arquivo: ");
+                    String nome = scanner.nextLine();
+                    descritor = cliente.Abre(nome);
+                    break;
+
+                case 2: // Le
+                    System.out.print("Descritor: ");
+                    descritor = scanner.nextInt();
+                    System.out.print("Posicao inicial: ");
+                    int posicaoLe = scanner.nextInt();
+                    System.out.print("Tamanho da leitura: ");
+                    int tamanho = scanner.nextInt();
+                    scanner.nextLine();
+                    cliente.Le(descritor, posicaoLe, tamanho);
+                    break;
+
+                case 3: // Escreve
+                    System.out.print("Descritor: ");
+                    descritor = scanner.nextInt();
+                    System.out.print("Posicao inicial: ");
+                    int posicaoEscreve = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.print("Conteudo para escrever: ");
+                    String conteudo = scanner.nextLine();
+                    cliente.Escreve(descritor, posicaoEscreve, conteudo);
+                    break;
+
+                case 4: // Fecha
+                    System.out.println("Descritor: ");
+                    descritor = scanner.nextInt();
+                    cliente.Fecha(descritor);
+                    break;
+
+                case 0: // Sair
+                    System.out.println("Encerrando cliente...");
+                    scanner.close();
+                    return;
+
+                default:
+                    System.out.println("Opcao invalida!");
+            }
+            Thread.sleep(2000);
+        }
     }
 }
--- java/src/main/java/com/mycompany/ine5418/client_lib/ClienteSistemaArquivos.java
+++ java/src/main/java/com/mycompany/ine5418/client_lib/ClienteSistemaArquivos.java
@@ -6,10 +6,14 @@ import com.mycompany.ine5418.SistemaArquivosProto;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ClienteSistemaArquivos {
     private final ManagedChannel channel;
     private final SistemaArquivosGrpc.SistemaArquivosBlockingStub userStub;
+    private final Map<Integer, Character> cache = new HashMap<>();
+    private int ultimoDescritor = -1;
 
     public ClienteSistemaArquivos(String host, int port) {
         this.channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build();
@@ -29,12 +33,42 @@ public class ClienteSistemaArquivos {
     }
 
     public int Le(int descritor, int posicao, int tamanho){
+        // se mudou o arquivo, reseta a cache
+        if (descritor != ultimoDescritor) {
+            cache.clear();
+            ultimoDescritor = descritor;
+        }
+
+        // percorre cada posicao desejada e verifica se esta presente na cache, evitando requisitar ao servidor
+        boolean cacheMiss = false;
+        char[] conteudo_cache = new char[tamanho];
+        for (int i = 0; i<tamanho; i++){
+            if (cache.get(posicao+i) == null) {
+                cacheMiss = true;
+                break;
+            }
+            conteudo_cache[i] = cache.get(posicao+i);
+        }
+        if (!cacheMiss){
+            System.out.println("Conteudo lido da cache: "+ new String(conteudo_cache));
+            return 0;
+        }
+
         SistemaArquivosProto.LeRequest request = SistemaArquivosProto.LeRequest.newBuilder()
                 .setDescritor(descritor).setPosicao(posicao).setTamanho(tamanho).build();
 
         SistemaArquivosProto.LeReply reply = userStub.le(request);
         if (reply.getStatus() >= 0){
             System.out.println("Conteudo lido: "+reply.getConteudoLer().toStringUtf8());
+
+
+            if (reply.getStatus() >= 0) {
+                byte[] dados = reply.getConteudoLer().toByteArray();
+                for (int i = 0; i < dados.length; i++) {
+                    cache.put(posicao + i, (char) dados[i]);
+                }
+            }
+
         } else {
             System.out.println("Nao foi possivel ler o conteudo. Status: "+reply.getStatus());
         }
@@ -42,6 +76,11 @@ public class ClienteSistemaArquivos {
     }
 
     public int Escreve(int descritor, int posicao, String conteudo){
+        // se mudou o arquivo, reseta a cache
+        if (descritor != ultimoDescritor) {
+            cache.clear();
+            ultimoDescritor = descritor;
+        }
         ByteString conteudoReq = ByteString.copyFrom(conteudo.getBytes(StandardCharsets.UTF_8));
 
         SistemaArquivosProto.EscreveRequest request = SistemaArquivosProto.EscreveRequest.newBuilder()
@@ -51,6 +90,13 @@ public class ClienteSistemaArquivos {
 
         if (reply.getStatus() >= 0){
             System.out.println("Bytes escritos: " + reply.getBytesEscritos());
+
+            // escreve o conteudo na cache tambem
+            char[] conteudo_cache = conteudo.toCharArray();
+            for (int i = 0; i<reply.getBytesEscritos(); i++){
+                cache.put(posicao+i, conteudo_cache[i]);
+            }
+
             return reply.getBytesEscritos();
         }
         System.out.println("Falha ao escrever. Status: "+reply.getStatus());
